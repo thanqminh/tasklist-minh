becc4800773be2f3adff37c045eb6eced0600812
Minh Than <thanqminh@gmail.com>
2022-12-24 05:29:35 +0000

    add env and sendgrid

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,18 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  ActionMailer::Base.smtp_settings = {
+      :address => 'smtp.sendgrid.net',
+      :port => '587',
+      :authentication => :plain,
+      :user_name => 'apikey',
+      :password => ENV['SENDGRID_APIKEY'],
+      :domain => 'dev.thanqminh.com',
+      :enable_starttls_auto => true
+    }
+
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.default_url_options ={:host => 'http://dev.thanqminh.com:3000'}
+
 end
